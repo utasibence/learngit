16b1e6838184e9209e12143c6e6e6c95bb64c802
Utasi Bence <utasibence@gmail.com>
2020-01-01 16:25:24 +0100

    Completed large hero block and intro content block

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+
+function cssInject() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+}
+
+function css() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,40 @@
-const gulp = require('gulp');
-const watch = require('gulp-watch');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssvars = require('postcss-simple-vars');
-const nested = require('postcss-nested');
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  console.log("Gulp task");
-});
-
-function html() {
-  console.log("html done");
+function cssInject() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 }
 
 function css() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, autoprefixer, nested]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 }
 
-gulp.task('watch', function(){
-  watch('./app/index.html', function() {
-    html();
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
   });
 
-  watch('./app/assets/styles/styles.css', function() {
+  watch('./app/assets/styles/**/*.css', function() {
     css();
+    cssInject();
+
+  });
+
+  watch('./app/index.html', function() {
+    browserSync.reload();
   });
 });
