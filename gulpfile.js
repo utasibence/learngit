559341c77fd59159d742c838662e8c9387a1d4f4
Utasi Bence <utasibence@gmail.com>
2020-01-04 21:22:25 +0100

    Completed automatic sprite generation

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins'),
-browserSync = require('browser-sync').create();
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import'),
+  mixins = require('postcss-mixins'),
+  browserSync = require('browser-sync').create(),
+  svgSprite = require('gulp-svg-sprite'),
+  rename = require('gulp-rename'),
+  del = require('del');
 
 function cssInject() {
   return gulp.src('./app/temp/styles/styles.css')
@@ -18,6 +21,55 @@ function css() {
     .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 }
+
+
+
+function createSprite() {
+
+  var config = {
+    mode: {
+      css: {
+        sprite: 'sprite.svg',
+        render: {
+          css: {
+            template: './gulp/templates/sprite.css'
+          }
+        }
+      }
+    }
+  }
+
+  console.log("2");
+
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite/'));
+}
+
+function copySpriteCSS() {
+  console.log("3");
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+}
+
+function copySpriteGraphic() {
+  console.log("4");
+  return gulp.src('./app/temp/sprite/css/**/*.svg')
+    .pipe(gulp.dest('./app/assets/images/sprites/'));
+}
+
+function clean() {
+  console.log("1");
+  del(['./app/temp/sprite', './app/assets/images/sprites']);
+}
+
+gulp.task('icons', async function() {
+  clean();
+  createSprite();
+  copySpriteCSS();
+  copySpriteGraphic();
+});
 
 gulp.task('watch', function() {
 
@@ -31,7 +83,6 @@ gulp.task('watch', function() {
   watch('./app/assets/styles/**/*.css', function() {
     css();
     cssInject();
-
   });
 
   watch('./app/index.html', function() {
